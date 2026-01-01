2e34144d6a725ccfb5ea916e7410ecb408c234e6
Richard Muraguri <muraguri2005@gmail.com>
2026-01-01 17:44:15 +0300

    Use ksp

--- app/src/main/kotlin/org/freelesson/pagingsample/api/RepoSearchResponse.kt
+++ app/src/main/kotlin/org/freelesson/pagingsample/api/RepoSearchResponse.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 
 class RepoSearchResponse(
     @SerializedName("total_count") var total: Int = 0,
-    @SerializedName("items") var items: List<Repo> = emptyList<Repo>(),
+    @SerializedName("items") var items: List<Repo> = emptyList(),
     var nextPage: Int? = null,
 )
